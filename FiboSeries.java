11e92545128a9950f2b13c75cea9840bd73d04d3
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-06-23 23:37:57 +0530

    Add files via upload

---
+++ FiboSeries.java
@@ -0,0 +1,24 @@
+package loop_practice;
+
+import java.util.Scanner;
+
+public class FiboSeries {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int a = 0;
+		int b = 1;
+		for(int i = 0; i < n; i++) {
+			System.out.print(a + " ");
+			int c = a+b;
+			a = b;
+			b = c;
+		}
+		System.out.print(a);
+
+	}
+
+}
+
