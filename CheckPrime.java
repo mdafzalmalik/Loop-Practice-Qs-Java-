5aa1643e5b59a9911fa0b31f359ee3c001c52675
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-06-23 23:37:40 +0530

    Add files via upload

---
+++ CheckPrime.java
@@ -0,0 +1,26 @@
+package loop_practice;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int count = 0; 
+		for(int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				count++;
+				break;
+			}
+		}
+		if(count >= 1) {
+			System.out.print("Not a Prime no.");
+		} else {
+			System.out.print("Prime no.");
+		}
+
+	}
+
+}
