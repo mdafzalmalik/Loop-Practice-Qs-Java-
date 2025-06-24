256089dead8ff7f119797eb40edf1277dae9f2a7
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-06-24 21:30:19 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,21 @@
+package loop_practice;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int rev = 0;
+		while(n > 0) {
+			int rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n/ 10;
+		}
+		System.out.print(rev);
+
+	}
+
+}
